7f833310a06120c9b5c4ffa1214bb2fe18abe6a2
Ashok326-del <mamillapalliashok2468@gmail.com>
2025-11-07 11:16:04 +0530

    dev2 code added tho the file

--- app.java
+++ app.java
@@ -3,3 +3,15 @@ public class HelloWorld {
         System.out.println("Hello, Java! Welcome to programming.");
     }
 }
+
+//developer 2 code starts from here sum of two numbers
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
